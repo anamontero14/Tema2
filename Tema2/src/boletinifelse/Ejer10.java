a0fb0d4b25cf526da020addccc36f464f4e257ce
USUARIO <USUARIO@DESKTOP-VG0NMFQ>
2024-10-08 21:09:24 +0200

    Ejercicio 10 acabado

--- Tema2/src/boletinifelse/Ejer10.java
+++ Tema2/src/boletinifelse/Ejer10.java
@@ -18,7 +18,10 @@ public class Ejer10 {
 		int num1;
 		int num2;
 		int num3;
-		
+
+		// Variable que albergará la suma de dos de los números
+		int suma;
+
 		// Variable que almacenará el valor de la media
 
 		// Creo el Scanner
@@ -31,8 +34,15 @@ public class Ejer10 {
 		num1 = leer.nextInt();
 		num2 = leer.nextInt();
 		num3 = leer.nextInt();
-		
-		
+
+		// Hago la suma a dos de los números introducidos y se lo asigno a la variable
+		suma = num1 + num2;
+
+		if (suma == num3) {
+			System.out.println("La suma de los dos primeros números es igual que el tercer número introducido");
+		} else {
+			System.out.println("La suma de los dos primeros números NO es igual que el tercer número introducido");
+		}
 
 		// Cierro el Scanner
 		leer.close();
