95bb35105c95f0d5fa3cb09dd544ab8887a9f297
USUARIO <USUARIO@DESKTOP-VG0NMFQ>
2024-10-16 15:29:56 +0200

    Ejercicio 6 acabado

--- Tema2/src/boletinbucles/Ejer6.java
+++ Tema2/src/boletinbucles/Ejer6.java
@@ -1,5 +1,7 @@
 package boletinbucles;
 
+import java.util.Scanner;
+
 public class Ejer6 {
 
 	public static void main(String[] args) {
@@ -11,6 +13,43 @@ public class Ejer6 {
 		 * cuando se utilice -1 como altura). La aplicación debe devolver la altura del
 		 * árbol más alto
 		 */
+		
+		// Altura de árbol
+		double altura;
+		
+		// Árbol más alto
+		double  másAlto;
+		
+		// Inicialización de Scanner
+		Scanner leer = new Scanner(System.in);
+		
+		// Le digo al usuario lo que tiene que hacer
+		System.out.println("Introduzca la altura del árbol:");
+		
+		// Le asigno el dato introducido a la variable altura
+		altura = leer.nextDouble();
+		
+		// Le asigno el valor de la variable altura a másAlto
+		másAlto = altura;
+		
+		while (altura > -1) {
+			
+			if (altura > másAlto) {
+				másAlto = altura;
+			}
+			
+			// Le digo al usuario lo que tiene que hacer
+			System.out.println("Introduzca la altura del árbol:");
+			
+			// Le asigno el dato introducido a la variable altura
+			altura = leer.nextInt();
+			
+		}
+		
+		System.out.println("El árbol más alto mide: " + másAlto + "cm");
+		
+		// Cierre Scanner
+		leer.close();
 
 	}
 
